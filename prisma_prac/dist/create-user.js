f9e35854da6ac1c3adc455363ccbcac189e80de1
YashBarbole <yashbarbole7110@gmail.com>
2026-02-09 14:45:29 +0530

    prima full connection done

---
+++ prisma_prac/dist/create-user.js
@@ -0,0 +1,40 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+async function main() {
+    // Create a new user with a post
+    const user = await prisma.user.create({
+        data: {
+            name: 'Alice',
+            email: 'alice@prisma.io',
+            posts: {
+                create: {
+                    title: 'Hello World',
+                    content: 'This is my first post!',
+                    published: true,
+                },
+            },
+        },
+        include: {
+            posts: true,
+        },
+    });
+    console.log('Created user:', user);
+    // Fetch all users with their posts
+    const allUsers = await prisma.user.findMany({
+        include: {
+            posts: true,
+        },
+    });
+    console.log('All users:', JSON.stringify(allUsers, null, 2));
+}
+main()
+    .catch((e) => {
+    console.error(e);
+    process.exit(1);
+})
+    .finally(async () => {
+    await prisma.$disconnect();
+});
+//# sourceMappingURL=create-user.js.map
